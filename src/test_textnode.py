bc0a09a382664493d7bc15573d73f87aa287c6de
memcmahon <mcmahon.meganelizabeth@gmail.com>
2025-06-03 17:20:35 -0600

    add function to convert text to html

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     # init
@@ -25,6 +25,12 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a image node", TextType.IMAGE, "http://test.com")
         self.assertEqual(repr(node1), "TextNode(This is a text node, bold, None)")
         self.assertEqual(repr(node2), "TextNode(This is a image node, image, http://test.com)")
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -10,9 +11,9 @@ class TextType(Enum):
 
 class TextNode:
     def __init__(self, text, text_type: TextType, url = None):
-            self.text = text
-            self.text_type = TextType(text_type)
-            self.url = url
+        self.text = text
+        self.text_type = TextType(text_type)
+        self.url = url                
     
     def __eq__(self, other):
         return (
@@ -23,4 +24,20 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-    
+
+def text_node_to_html_node(self):
+    match self.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, self.text)
+        case TextType.BOLD:
+            return LeafNode("b", self.text)
+        case TextType.ITALIC:
+            return LeafNode("i", self.text)
+        case TextType.CODE:
+            return LeafNode("code", text)
+        case TextType.LINK:
+            return LeafNode("a", self.text, {"href": self.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": self.url, "alt": self.text})
+        case _:
+            raise Exception("unknown type")
