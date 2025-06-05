a47cf509ce82ce6c7668dcfdd64ae00ee8870cdd
memcmahon <mcmahon.meganelizabeth@gmail.com>
2025-06-04 18:19:25 -0600

    correct import structure

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from htmlnode import ParentNode
-from inline_markdown import text_to_textnodes
+from extractors import text_to_textnodes
 from textnode import text_node_to_html_node, TextNode, TextType
 
 
