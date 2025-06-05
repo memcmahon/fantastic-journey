5f482ba60a9757575073a4c1c49267ae06c75b8d
memcmahon <mcmahon.meganelizabeth@gmail.com>
2025-06-05 10:20:25 -0600

    static site generator complete

--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+from contentgenerator import generate_pages
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_pages(dir_path_content, template_path, dir_path_public)
+
 
 main()
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
-from htmlnode import ParentNode
-from extractors import text_to_textnodes
+from parentnode import ParentNode
+from delimiter import text_to_textnodes
 from textnode import text_node_to_html_node, TextNode, TextType
 
 
